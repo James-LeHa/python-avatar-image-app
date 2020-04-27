d8bb8dd672a7e84765d3fdaeac373144cc7f20c3
James Ha <james-leha@github.com>
2020-04-26 22:50:59 -0500

    settings update, js enter key input accepts

--- codeApp.py
+++ codeApp.py
@@ -2,10 +2,14 @@ import requests
 import settings
 import web
 import os
+import socket
 
+localHost = socket.gethostname()
 
-#headers = {"Authorization": "Bearer " + settings.key}
-headers = {"Authorization": "Bearer " + os.environ['APIKEY']}
+if (localHost == settings.my_hostname):
+    headers = {"Authorization": "Bearer " + settings.key}
+else:
+    headers = {"Authorization": "Bearer " + os.environ['APIKEY']}
 
 def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 key = os.getenv('APIKEY')
+my_hostname = os.getenv('my_hostname')
